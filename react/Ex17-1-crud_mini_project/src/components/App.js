61dddc49dbdb162a3acf3d830a244e9bd5528269
reuttol <reut.tole@gmail.com>
2021-12-23 09:04:59 +0200

    CRUD mini project update

--- react/Ex17-1-crud_mini_project/src/components/App.js
+++ react/Ex17-1-crud_mini_project/src/components/App.js
@@ -7,13 +7,33 @@ import "./app.css";
 export default class App extends Component {
   state = {users: []};
 
-  async componentDidMount(){
+  getData = async () =>{
     const res = await axios.get('https://61c40cf8f1af4a0017d99240.mockapi.io/reut-avatar/users');
     this.setState({users: res.data});
   }
 
+  async componentDidMount(){
+    await this.getData();
+  }
+
+  delete = async (userID) =>{
+    const res = await axios.delete(`https://61c40cf8f1af4a0017d99240.mockapi.io/reut-avatar/users/${userID}`);
+    this.setState((prevState)=> ({
+      users: prevState.users.filter(user => user.id !== userID)
+    }));
+  }
+
+  edit = async(userID, userName) =>{
+    const tempUsers = this.state.users;
+    const user = tempUsers.filter(user => userID.id === userID);
+    user.name = userName;
+    await axios.put(`https://61c40cf8f1af4a0017d99240.mockapi.io/reut-avatar/users/${userID}`,{...user});
+    await this.getData();
+   
+  }
+
   renderUsers = () => {
-    return this.state.users.map((user=> <UserCard user={user}/>));
+    return this.state.users.map((user=> <UserCard key={user.id} user={user} handleEditClick={this.edit} handleDeleteClick={this.delete}/>));
   }
   
   render() {
--- react/Ex17-1-crud_mini_project/src/components/UserCard.js
+++ react/Ex17-1-crud_mini_project/src/components/UserCard.js
@@ -2,15 +2,21 @@ import React, { Component } from "react";
 import "./app.css";
 
 export default class UserCard extends Component {
+  
+  state = {value: this.props.user.name}
+  
   render() {
-    console.log("here", this.props.user);
     return (
       <div className="card">
         <div>{this.props.user.name}</div>
         <img src={this.props.user.imageURL} alt={this.props.user.name} />
         <div className="btns-container">
-          <button>Edit</button>
-          <button>Delete</button>
+        <input
+          value={this.state.value}
+          onChange={(e) => this.setState({ value: e.target.value })}
+        />
+          <button onClick={()=> this.props.handleEditClick(this.props.user.id, this.state.value)}>Edit</button>
+          <button onClick={()=> this.props.handleDeleteClick(this.props.user.id)}>Delete</button>
         </div>
       </div>
     );
